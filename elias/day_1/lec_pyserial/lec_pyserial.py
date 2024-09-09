56dee0c1d339e6ee5abde86dfe317176ea360333
Elias Kim <eliaskiss@hotmail.com>
2024-09-09 16:17:34 +0900

    Day1

--- elias/day_1/lec_pyserial/lec_pyserial.py
+++ elias/day_1/lec_pyserial/lec_pyserial.py
@@ -17,6 +17,7 @@ def openSerial(port, baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PAR
 
     # 시리얼 포트설정
     ser.port = port             # Port Name : com1, com2, ....
+    ser.stopbits = stopbits
     ser.baudrate = baudrate     # Baudrate 속도 : 9600, 115200, ...
     ser.bytesize = bytesize     # Data Bit
     ser.parity = parity         # Check Parity
@@ -42,15 +43,30 @@ def writePort(ser, data):
 def writePortUnicode(ser, data, encode='utf-8'):
     writePort(ser, data.encode(encode))
 
+################################################################
+# Read Port
+################################################################
+def read(ser, size=1, timeout=None):
+    ser.timeout = timeout
+    readed = ser.read(size)
+    return readed
+
+
 if __name__ == '__main__':
     # 포트열기
     ser = openSerial(port='com2')
 
-    # 포트쓰기
-    data = "HelloWorld"
-    enc_data = data.encode()
-    writePort(ser, enc_data)        # 인코딩 후 인자로 전달
-    writePortUnicode(ser, data)     # 인코딩 없이 인자로 전달
+    # # 포트쓰기
+    # data = "HelloWorld"
+    # enc_data = data.encode()
+    # writePort(ser, enc_data)        # 인코딩 후 인자로 전달
+    # writePortUnicode(ser, data)     # 인코딩 없이 인자로 전달
+
+    # # 1 byte만 읽기
+    # ic(read(ser))
+
+    # 10 byte 읽기
+    ic(read(ser, 10))
 
     # time.sleep(10)
 
