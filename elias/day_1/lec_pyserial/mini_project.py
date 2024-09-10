dc593bd1aa5cbb1f2b3040bd215eeb3819293c96
Elias Kim <eliaskiss@hotmail.com>
2024-09-11 08:46:59 +0900

    Day3

--- elias/day_1/lec_pyserial/mini_project.py
+++ elias/day_1/lec_pyserial/mini_project.py
@@ -6,34 +6,38 @@ from icecream import ic
 # Exit, exit, EXIT, eXit, EXit lower() 들어오면 프로그램 종료`
 # b'\x0d'
 
-ser = MySerial()
-ser.openSerial('com2')
+def main():
+    ser = MySerial()
+    ser.openSerial('com2')
 
-ic('Echo Server is running...')
+    ic('Echo Server is running...')
 
-RETURN_CODE = b'\x0d'
+    RETURN_CODE = b'\x0d'
 
-while True:
-    # Enter키가 입력될때까지 데이터를 Read
-    readed = ser.readUntilExitCode(RETURN_CODE)
-    ic(readed)
+    while True:
+        # Enter키가 입력될때까지 데이터를 Read
+        readed = ser.readUntilExitCode(RETURN_CODE)
+        ic(readed)
 
-    # byte array --> Unicode String Object
-    # 문자열 객체(Unicode)로 Decode 해야만 문자열 관련 내장함수 사용가능
-    readed = readed.decode()
-    ic(readed)
+        # byte array --> Unicode String Object
+        # 문자열 객체(Unicode)로 Decode 해야만 문자열 관련 내장함수 사용가능
+        readed = readed.decode()
+        ic(readed)
 
-    # 프로그램 종료조건(exit) 확인
-    if readed.lower() == 'exit':
-        ic('Done')
-        ser.writePortUnicode('Echo server is dead.\r\n')
-        break
+        # 프로그램 종료조건(exit) 확인
+        if readed.lower() == 'exit':
+            ic('Done')
+            ser.writePortUnicode('Echo server is dead.\r\n')
+            break
 
-    response = readed + '\r\n'
-    ic(response)
-    ser.writePortUnicode(response)
+        response = readed + '\r\n'
+        ic(response)
+        ser.writePortUnicode(response)
 
-ser.closePort()
+    ser.closePort()
+
+if __name__ == '__main__':
+    main()
 
 
 
