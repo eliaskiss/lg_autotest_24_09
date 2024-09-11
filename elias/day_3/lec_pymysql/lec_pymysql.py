5a3801d8df3d9cd84d4939bbf696c200f34c7de6
Elias Kim <eliaskiss@hotmail.com>
2024-09-11 14:43:57 +0900

    Day3

--- elias/day_3/lec_pymysql/lec_pymysql.py
+++ elias/day_3/lec_pymysql/lec_pymysql.py
@@ -19,6 +19,25 @@ class Database:
         self.cursor = None  # Cursor 객체
         self.conn = None    # Connection 객체
 
+    ################################################################
+    # Connect DB
+    ################################################################
+    def connect_db(self):
+        try:
+            if self.conn is None:
+                self.conn = pymysql.connect(host=self.host,
+                                            user=self.user,
+                                            password=self.passwd,
+                                            db=self.db)
+                self.cursor = self.conn.cursor()    # row[0], row[1], row[2], ...
+                self.cursor = self.conn.cursor(pymysql.cursors.DictCursor) # row['id'], row['reg_datetime'], row['name'], ...
+
+            ic('DB is connected')
+        except Exception as e:
+            message = f'--> Exception is {e} (Line: {sys.exc_info()[-1].tb_lineno})'
+            ic(message)
+
+
 
 
 
