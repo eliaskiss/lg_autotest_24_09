95391b33fe44d62dba6e95341a2fd45c90e40011
Elias Kim <eliaskiss@hotmail.com>
2024-09-12 13:55:21 +0900

    Day4

--- elias/day_4/lec_opencv/lec_opencv.py
+++ elias/day_4/lec_opencv/lec_opencv.py
@@ -2,12 +2,15 @@ import cv2
 from icecream import ic
 from datetime import datetime
 
+from elias.day_4.lec_pyqt5.lec_pyqt5 import filename
+
+
 class WebCam:
     def __init__(self, portNum=None):
         self.port_num = portNum
 
     #################################################################
-    # GEt Webcam List
+    # Get Webcam List
     #################################################################
     def get_valid_camera_list(self, max_port_num=3):
         camera_port_list = []
@@ -22,6 +25,38 @@ class WebCam:
                 break
 
         return camera_port_list
+
+    #################################################################
+    # Set Port Number
+    #################################################################
+    def set_port(self, portNum):
+        self.port_num = portNum
+
+    #################################################################
+    # Capture Webcam Image
+    #################################################################
+    def capture_image(self, file_name, width=1280, height=720):
+        # 웹캠 객체생성
+        cap = cv2.VideoCapture(self.port_num, cv2.CAP_DSHOW)
+
+        # 웹캠 옵션설정
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        cap.set(cv2.CAP_PROP_AUTOFOCUS, 1)
+
+        # 카메라 이미지 캡쳐
+        _, frame = cap.read()
+
+        # 캡쳐된 이미지를 파일로 저장
+        ret = cv2.imwrite(file_name, frame)
+
+        # 객체핸들 릴리즈
+        cap.release()
+
+        return ret, file_name
+
+
+
 
 if __name__ == '__main__':
     cam = WebCam()
