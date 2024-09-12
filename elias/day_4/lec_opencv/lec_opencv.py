36a74dd3f8f2d94be7b9de548ccb1e54bad6111d
Elias Kim <eliaskiss@hotmail.com>
2024-09-12 14:25:43 +0900

    Day4

--- elias/day_4/lec_opencv/lec_opencv.py
+++ elias/day_4/lec_opencv/lec_opencv.py
@@ -91,6 +91,58 @@ class WebCam:
         # cv2.destroyAllWindows()
         cv2.destroyWindow('frame')
 
+    #################################################################
+    # Record Video Stream with AVI Codec
+    #################################################################
+    def record_video(self, video_file_name, width=1280, height=720, flip=None, fps=24):
+        # 웹캠 객체생성
+        cap = cv2.VideoCapture(self.port_num, cv2.CAP_DSHOW)
+
+        # 웹캠 옵션설정
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        cap.set(cv2.CAP_PROP_AUTOFOCUS, 1)
+
+        # 코덱설정
+        # fourcc = cv2.VideoWriter_fourcc('D', 'I', 'V', 'X')
+        fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+
+        # 실제 적용된 크기
+        frame_size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                      int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+        
+        # Writer 객체
+        out = cv2.VideoWriter(f'{video_file_name}.avi', fourcc, fps, frame_size)
+
+        while cap.isOpened():
+            # 현재 영상 캡쳐
+            ret, frame = cap.read()
+
+            # 캡쳐 실패시 실행중단
+            if ret is False:
+                break
+
+            # 플립적용
+            if flip is not None:
+                # flip: 0 -> bottom to top, 1 -> left to right
+                frame = cv2.flip(frame, flip)
+
+            # 윈도우화면에 영상출력
+            cv2.imshow('frame', frame)
+
+            # 동영상파일에 Frame 저장
+            out.write(frame)
+
+            if cv2.waitKey(int(1000/fps)) == ord('q'):
+                break
+
+        # 객체핸들 릴리즈
+        cap.release()
+        # cv2.destroyAllWindows()
+        cv2.destroyWindow('frame')
+
+
+
 
 
 if __name__ == '__main__':
@@ -103,11 +155,21 @@ if __name__ == '__main__':
         port_num = port_list[0]
         cam.set_port(port_num)
 
-        ################################################################
-        # Capture Image(Snapshot)
-        ################################################################
-        file_name = f'{datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.png'
-        ic(cam.capture_image(file_name))
+        # ################################################################
+        # # Capture Image(Snapshot)
+        # ################################################################
+        # file_name = f'{datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.png'
+        # ic(cam.capture_image(file_name))
+
+        # ################################################################
+        # # Capture Video Stream
+        # ################################################################
+        # cam.capture_video()
+        # cam.capture_video(800, 600)
+        # cam.capture_video(isMono=True)
+        # cam.capture_video(flip=0)
+        # cam.capture_video(flip=1)
+        # cam.capture_video(isMono=True, flip=1)
 
         
         
